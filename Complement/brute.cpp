e6f66acee82332806ddc9799a799f7b6f5d9af07
Matheus Fernandes Rabelo <104003912+suehtaMFr@users.noreply.github.com>
2025-05-29 10:19:01 -0300

    Update brute.cpp

--- Complement/brute.cpp
+++ Complement/brute.cpp
@@ -4,14 +4,7 @@ using namespace std;
 
 typedef long long ll;
 
-#define pb push_back
-#define sz(x) ((int)x.size())
-
 int main (){
-  ll y, k, x = 1; cin >> y >> k;
-  for(ll i=0;i<k;i++){
-    x += __gcd(x,y);
-  }
-  cout << x << "\n";
+  //A code that solves the problem, but it is inefficient in terms of time complexity, memory usage, or both.
 }
 
